150549015063364de6d3a8f49daff0d5397f5ae1
Bruce Hauman <bhauman@gmail.com>
2016-06-12 17:45:05 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-lein-profile-merge "0.1.1"
+(defproject simple-lein-profile-merge "0.1.2"
   :description
   "A simple Clojure library to help fetch configuration information from your project.clj without needing to load Leiningen."
   :url "https://github.com/bhauman/simple-lein-profile-merge"
